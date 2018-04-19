fbbb2078ae13e8444a4a16b89de061af43b1c49d
Washington Alexandre da Silva <washington.silva@SPOMM010114173.local>
2018-04-19 15:45:37 -0300

    Subindo projeto de jogos

---
+++ Assets/Scripts/ArmaBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmaBehaviourScript : MonoBehaviour {
+
+	public GameObject projetilPrefab;
+	public float intervalo;
+
+	void Update () {
+		
+		if (Input.GetKeyDown ("f"))
+			//instancia na cena
+			Atirar();
+			
+	}
+
+	public void Atirar () {
+		Instantiate (projetilPrefab, transform.position, transform.rotation);
+	}
+		
+
+//	IEnumerator Start () {
+//		//aguarda para executar
+//		yield return new WaitForSeconds (intervalo);
+//		//instancia na cena
+//		Instantiate (projetilPrefab, transform.position, transform.rotation);
+//		//metodo chamado toda vez q for executado
+//		StartCoroutine(Start());
+//	}
+}
+
+
---
+++ Assets/Scripts/ProjetilHamburguerhaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjetilHamburguerhaviourScript : MonoBehaviour {
+
+	public float velocidade;
+	public float tempoDeVida;
+
+	// Use this for initialization
+	void Start () {
+		//tempo de vida do hamburguer
+		Destroy (gameObject, tempoDeVida);
+		//remove o efeito de gravidade do objeto
+		GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeAll;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//move o hamburguer
+		transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		print ("colidiiu com" + c.gameObject.tag); 
+		if (c.gameObject.tag == "Inimigo")
+			Destroy (gameObject);
+	}
+}
